4a800c0467139bdb19a9de3bbc5feabd8d56d685
Simon Gockner <simon.gockner@gmail.com>
2019-04-29 16:03:47 +0200

    - add multitonScopeAccessor

---
+++ CastleWindsorFactory/MultitonScopeAccessor.cs
@@ -0,0 +1,57 @@
+﻿//  
+// Author: simon.gockner
+// Created: 2019-04-25
+// $HeadURL$
+// $Date$
+// $Id$
+// Copyright(c) 2019 Buerkert GmbH&CoKG. All Rights Reserved.
+// 	
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Castle.MicroKernel.Context;
+using Castle.MicroKernel.Lifestyle.Scoped;
+
+namespace CastleWindsorFactory
+{
+    public class MultitonScopeAccessor<TScope> : IScopeAccessor where TScope : class
+    {
+        private static readonly ConditionalWeakTable<TScope, ILifetimeScope> _instances = new ConditionalWeakTable<TScope, ILifetimeScope>();
+        private static readonly object _instancesLockObject = new object();
+
+        /// <summary>
+        /// Gets the scope of the resolving object -> if an instance for <see cref="TScope"/> is already created return this, otherwise create a new instance
+        /// </summary>
+        /// <param name="context">The <see cref="CreationContext"/></param>
+        /// <returns>The existing instance or a new instance if none exists</returns>
+        /// <exception cref="ArgumentException">If no additional arguments are given</exception>
+        public ILifetimeScope GetScope(CreationContext context)
+        {
+            if (!context.HasAdditionalArguments)
+                throw new ArgumentException("No additional arguments given.");
+
+            var keyValuePair = context.AdditionalArguments.ToList().First();
+            if (!(keyValuePair.Value is TScope scope))
+                throw new ArgumentException($"First additional argument is not of type {typeof(TScope)} (Type: {context.AdditionalArguments[0].GetType()}).");
+
+            lock (_instancesLockObject)
+            {
+                if (_instances.TryGetValue(scope, out ILifetimeScope lifetimeScope))
+                    return lifetimeScope;
+
+                lifetimeScope = new DefaultLifetimeScope();
+                _instances.Add(scope, lifetimeScope);
+
+                return lifetimeScope;
+            }
+        }
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
