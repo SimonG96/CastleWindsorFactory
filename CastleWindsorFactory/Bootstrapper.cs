5a689e54c8c9302cd90b4a3e99ff2cec8b21e9db
Simon Gockner <simon.gockner@gmail.com>
2019-05-21 16:35:30 +0200

    - update bootstrapper

--- CastleWindsorFactory/Bootstrapper.cs
+++ CastleWindsorFactory/Bootstrapper.cs
@@ -1,21 +1,20 @@
 ﻿using System.Linq;
-using Castle.Facilities.TypedFactory;
-using Castle.MicroKernel.ModelBuilder.Inspectors;
-using Castle.Windsor;
 using CastleWindsorFactory.Installers;
+using DependencyInjector;
+using DependencyInjector.Interfaces;
 
 namespace CastleWindsorFactory
 {
     public class Bootstrapper
     {
-        public IWindsorContainer BootstrapContainer()
+        public IInjectorContainer BootstrapContainer()
         {
-            WindsorContainer kernel = new WindsorContainer();
-            kernel.AddFacility<TypedFactoryFacility>();
+            InjectorContainer kernel = new InjectorContainer();
+            //kernel.AddFacility<TypedFactoryFacility>();
 
-            //disable property injection, only constructor injection is allowed
-            PropertiesDependenciesModelInspector propertyInjector = kernel.Kernel.ComponentModelBuilder.Contributors.OfType<PropertiesDependenciesModelInspector>().Single();
-            kernel.Kernel.ComponentModelBuilder.RemoveContributor(propertyInjector);
+            //TODO: disable property injection, only constructor injection is allowed
+            //PropertiesDependenciesModelInspector propertyInjector = kernel.Kernel.ComponentModelBuilder.Contributors.OfType<PropertiesDependenciesModelInspector>().Single();
+            //kernel.Kernel.ComponentModelBuilder.RemoveContributor(propertyInjector);
 
             return kernel.Install(new Installer(),
                 new FooInstaller(),
