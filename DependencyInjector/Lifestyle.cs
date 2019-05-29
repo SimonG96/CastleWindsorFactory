bd0aeabf84633fe5dcf00bec9e7c48d43e8ce9a7
Simon Gockner <simon.gockner@gmail.com>
2019-05-29 16:41:51 +0200

    - add comments to lifestyle

--- DependencyInjector/Lifestyle.cs
+++ DependencyInjector/Lifestyle.cs
@@ -2,12 +2,25 @@
 // Created: 2019-05-20
 // Copyright(c) 2019 SimonG. All Rights Reserved.
 
+using DependencyInjector.Interfaces.Registrations;
+
 namespace DependencyInjector
 {
+    /// <summary>
+    /// The Lifestyles that can be used for a <see cref="IDefaultRegistration{TInterface}"/>
+    /// </summary>
     public enum Lifestyle
     {
+        /// <summary>
+        /// A new instance gets created every time an instance is resolved
+        /// </summary>
         Transient,
+
+        /// <summary>
+        /// One instance is created that gets returned every time an instance is resolved
+        /// </summary>
         Singleton
+
         //TODO: Add Lifestyle.Multiton
     }
 }
